84066d16c859d2bd5162ec4df96898f06b42d2b8
fhex <tomialenius@hotmail.com>
2022-01-30 15:23:31 +0100

    ColliderWallsTeleport

--- Assets/BorderWall.cs
+++ Assets/BorderWall.cs
@@ -5,25 +5,59 @@ using UnityEngine;
 public class BorderWall : MonoBehaviour
 {
     GameObject outofBoundTxt;
-
+    [SerializeField] public Vector3 CheckpointLocation;
+    [SerializeField] public Quaternion checkpointRot;
+    [SerializeField] public GameObject player;
     private void Start()
     {
         outofBoundTxt = GameObject.FindGameObjectWithTag("OutOfBounds");
         outofBoundTxt.SetActive(false);
+        CheckpointLocation = new Vector3(0, 0, 0);
+        checkpointRot = Quaternion.identity;
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (outofBoundTxt != null)
+        HitOutOFBounds(collision);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //HitOutOFBounds(other);
+    }
+
+    private void HitOutOFBounds(Collision collision)
+    {
+        Debug.Log(collision);
+        if (collision.gameObject.tag == "Player")
         {
-            outofBoundTxt.SetActive(true);
-            StartCoroutine(WaitForSenconds());
+
+            if (outofBoundTxt != null)
+            {
+                outofBoundTxt.SetActive(true);
+                if (CheckpointLocation != null)
+                {
+
+                    //collision.gameObject.transform.position = CheckpointLocation;
+                    StartCoroutine(WaitForSenconds(collision));
+                }
+            }
         }
         else return;
     }
-    IEnumerator WaitForSenconds()
-    {
-        yield return new WaitForSeconds(3);
-        outofBoundTxt.SetActive(false);
 
+    
+    IEnumerator WaitForSenconds(Collision collision)
+    {
+
+        yield return new WaitForSeconds(3);
+        TeleportPlayerToCheckPoint();
+
+    }
+
+    private void TeleportPlayerToCheckPoint()
+    {
+       // player = GameObject.FindGameObjectWithTag("Player");
+        player.transform.position = new Vector3(CheckpointLocation.x, CheckpointLocation.y + 5, CheckpointLocation.z);
+        player.transform.rotation = checkpointRot;
+        outofBoundTxt.SetActive(false);
     }
 }
--- Assets/Scripts/CheckPointDestroyer.cs
+++ Assets/Scripts/CheckPointDestroyer.cs
@@ -6,23 +6,31 @@ public class CheckPointDestroyer : MonoBehaviour
 {
     [SerializeField] GameObject CheckPointBurst;
     public bool activated = false;
-    
+    [SerializeField] private GameObject player;
+    [SerializeField] public Vector3 CheckpointLocation;
+    [SerializeField] public Quaternion checkpointRot;
 
     private void Start()
     {
-        
-       // transform.position = new Vector3(transform.position.x, Terrain.activeTerrain.SampleHeight(transform.position)+2,transform.position.z);
+        player = GameObject.FindGameObjectWithTag("Player");
+        // transform.position = new Vector3(transform.position.x, Terrain.activeTerrain.SampleHeight(transform.position)+2,transform.position.z);
         //gameObject.transform.position.y = Terrain.activeTerrain.SampleHeight(transform.position);
     }
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("CheckPoint");
-        //activated = true;
-        
-        FindObjectOfType<CheckPointHandler>().CheckpointTaken();
-        GameObject Firework = Instantiate(CheckPointBurst, transform.position, Quaternion.identity);
-        Destroy(Firework, 2);
-        Destroy(gameObject);
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("CheckPoint");
+            CheckpointLocation = new Vector3(other.transform.parent.position.x, other.transform.parent.position.y, other.transform.parent.position.z);
+            //checkpointRot = Quaternion.Euler(other.transform.rotation.x, other.transform.rotation.y, other.transform.rotation.z);
+            checkpointRot = player.transform.rotation;
+            FindObjectOfType<BorderWall>().CheckpointLocation = CheckpointLocation;
+            FindObjectOfType<BorderWall>().checkpointRot = checkpointRot;
+            FindObjectOfType<CheckPointHandler>().CheckpointTaken();
+            GameObject Firework = Instantiate(CheckPointBurst, transform.position, Quaternion.identity);
+            Destroy(Firework, 2);
+            Destroy(gameObject);
+        }
     }
 
 }
